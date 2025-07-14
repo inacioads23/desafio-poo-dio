f93244dcfd12f731d6012e1075409b7736233c0a
inacioads23 <jinacio11@gmail.com>
2025-07-14 12:55:24 -0300

    Finalizando os métodos da Class Dev

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,11 +1,14 @@
 // Usando 'Set' pois não é permitido mais de uma inscrição no mesmo conteúdo/ curso
 // Usando 'LinkedHashSet' para manter a ordenação das inscrições dos conteúdos
 // Uso do polimorfismo 'Set' -> 'LinkedHashSet'. Evita acoplamento
+/* '.stream()' é um método usado para criar um fluxo de dados (Stream) a partir de uma coleção, como List, Set ou Map.
+	Esse fluxo permite processar dados de forma funcional (sem loops explícitos) com métodos como filter(), map(), collect(), etc. */
 
 package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -15,12 +18,29 @@ public class Dev {
 	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 	
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-		
+		this.conteudosInscritos.addAll(bootcamp.getConteudos()); // Adicionado em 'conteudosInscritos' todo o conteúdo do 'bootcamp'
+		bootcamp.getDevsInscritos().add(this); // Adicionei o 'dev' Atual (this) ao 'bootcamp'
 	}
 	
-	public void progredir() {}
+	public void progredir() {
+		// Uso do 'Optional' para lidar com retorno 'null'
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		// se existir conteúdo
+		if(conteudo.isEmpty()) {
+			this.conteudosConcluidos.add(conteudo.get()); // adiciono o 'conteudo' a 'conteudosConcluidos'
+			this.conteudosInscritos.remove(conteudo.get()); // removo o 'conteudo' de 'conteudosInscritos'
+		}else {
+			System.err.println("Você não está matriculado em nenhum curso!");
+		}
+	}
 	
-	public void calcularTotalXp() {}
+	public double calcularTotalXp() {
+		return this.conteudosConcluidos
+				.stream()
+				.mapToDouble(conteudo -> conteudo.calcularXp()) // peguei cada 'conteudo' de dentro do Set-> peguei o 'Xp' de cada conteúdo
+				.sum(); // Fiz a soma dos 'Xps'
+		
+	}
 	
 
 	public String getNome() {
